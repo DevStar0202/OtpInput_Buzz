e7d72e2f19b055d7b271f54927c1357f5cbc2d15
David Morgan <111621165+DevStar0202@users.noreply.github.com>
2022-12-10 16:30:21 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import OtpInput from './Components/OtpInput';
 
-function App() {
+export default function App() {
+  const [otp, setOtp] = useState('');
+  const onChange = (value) => setOtp(value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>React JavaScript OTP Input devstar0202</h1>
+      <br/>
+      <br/>
+      <OtpInput value={otp} valueLength={6} onChange={onChange} />
+      <br/>
+      <br/>
+      <h2>New features.</h2>
+      <h3>1. autofocus</h3>
+      <h3>2. delete key</h3>
+      <h3>3. arrow right & left key</h3>
+      <h3>4. select</h3>
+      <h3>5. only delete when the next values is 'null'</h3>
+      <h3>6. ctrl + c and ctrl + v</h3>
+      <h3>7. only number</h3>
+      <h3>8. change Length of numbers (now: 6)</h3>
     </div>
   );
 }
-
-export default App;
